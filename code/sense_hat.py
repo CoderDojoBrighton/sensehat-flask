a550e175e92784d84f19f0ae6de48704bc599fe5
Bradley Garrod <bradleybredgarrod@gmail.com>
2024-01-25 17:24:55 +0000

    feat: Add other temperature methods in sense_hat

--- code/sense_hat.py
+++ code/sense_hat.py
@@ -4,5 +4,11 @@ class SenseHat():
     def get_temperature(self):
         return random.uniform(-5, 30)
 
+    def get_temperature_from_humidty(self):
+        return random.uniform(-5, 30)
+
+    def get_temperature_from_pressure(self):
+        return random.uniform(-5, 30)
+
     def get_humidty(self):
         return random.uniform(0, 100)
